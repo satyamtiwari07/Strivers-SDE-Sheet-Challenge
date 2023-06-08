21f8309c80a6ed6a0d8998158af2c5cca607d9c2
Satyam Tiwari <89099002+satyamtiwari07@users.noreply.github.com>
2023-06-08 17:49:03 +0530

    Create Floor_in_BST.cpp

---
+++ Floor_in_BST.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int floorInBST(TreeNode<int> * root, int X)
+{
+    // Write your code here.
+    int ans=-1;
+    while(root){
+        if(root->val>X) root=root->left;
+        else{
+            ans=root->val;
+            root=root->right;
+        }
+    }
+    return ans;
+}
