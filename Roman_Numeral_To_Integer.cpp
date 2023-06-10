379c176ea37eed6d2eaf2a6cd71a1340edffdc34
Satyam Tiwari <89099002+satyamtiwari07@users.noreply.github.com>
2023-06-11 02:07:24 +0530

    Create Roman_Numeral_To_Integer.cpp

---
+++ Roman_Numeral_To_Integer.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+int romanToInt(string s) {
+    // Write your code here
+    unordered_map<char,int>mpp;
+    mpp['I'] = 1;
+    mpp['V'] = 5;
+    mpp['X'] = 10;
+    mpp['L'] = 50;
+    mpp['C'] = 100;
+    mpp['D'] = 500;
+    mpp['M'] = 1000;
+    int ans=0;
+    int x=0;
+    int n=s.size();
+    for(int x=0;x<n;x++){
+      if (x+1<n and mpp[s[x]] < mpp[s[x + 1]]) {
+        ans += mpp[s[x + 1]] - mpp[s[x]];
+        x++;
+      } else
+        ans += mpp[s[x]];
+    }
+    // ans+=mpp[x]
+    return ans;
+}
